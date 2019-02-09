a109b19267837d242da9362764266f5959ec7758
UtonSenior <46914291+UtonSenior@users.noreply.github.com>
2019-02-09 14:09:34 -0500

    Recently Updated

--- vtdi_gate/GateInOut.Designer.cs
+++ vtdi_gate/GateInOut.Designer.cs
@@ -409,6 +409,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
             this.ClientSize = new System.Drawing.Size(867, 488);
             this.Controls.Add(this.tableLayoutPanel4);
             this.Controls.Add(this.tableLayoutPanel3);
